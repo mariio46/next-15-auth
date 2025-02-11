f1a4c1ff0bab104194b80de61256a622d512c293
Mario <mariomad2296@gmail.com>
2025-02-11 11:03:12 +0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+import { axiosClient } from './axios';
+
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export const fetcher = (url: string) => axiosClient.get(url).then((res) => res.data);
