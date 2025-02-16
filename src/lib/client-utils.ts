d026692e6d74227561d15196381b7e06edb76a24
Mario <mariomad2296@gmail.com>
2025-02-16 15:02:09 +0800

    Update client-utils.ts

--- src/lib/client-utils.ts
+++ src/lib/client-utils.ts
@@ -2,15 +2,28 @@
 
 import 'client-only';
 
+import type { AxiosError } from 'axios';
 import { getCookie } from 'cookies-next/client';
 import useSWR from 'swr';
 
 import type { AuthUser } from '@/types/api/auth';
 
+import { useAuthUserStore } from '@/stores/auth-user-store';
 import { fetcher } from './utils';
 
 export function getAuthUser() {
-    return useSWR<{ user: AuthUser }>('/api/auth-user', fetcher);
+    const { setAuth, clearAuth } = useAuthUserStore();
+
+    return useSWR<{ user: AuthUser }, AxiosError>(isLoggedIn() ? '/api/auth-user' : null, fetcher, {
+        onSuccess: (data) => {
+            setAuth(data.user);
+        },
+        onError: (e) => {
+            if (e.status === 401) {
+                clearAuth();
+            }
+        },
+    });
 }
 
 export function isLoggedIn(): boolean {
