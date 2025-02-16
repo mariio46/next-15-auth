33fef286f79fb7bb76f954c989eb41b726894eb3
Mario <mariomad2296@gmail.com>
2025-02-16 14:12:26 +0800

    Update server-utils.ts

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -2,6 +2,53 @@ import 'server-only';
 
 import { cookies } from 'next/headers';
 
+import type { AuthUser } from '@/types/api/auth';
+
+import { axiosServer } from './axios';
+import { getCurrentHostname } from './utils';
+
+export async function getAuthUser() {
+    const token = await bearerToken();
+
+    return await axiosServer.get<{ data: { user: AuthUser } }>('/api/auth/user', {
+        headers: {
+            Authorization: token,
+        },
+    });
+}
+
+const cookieExpires = new Date(Date.now() + 18000000);
+
+export async function setIsLoggedInCookie(value: string) {
+    const cookieStore = await cookies();
+
+    cookieStore.set({
+        name: 'isLoggedIn',
+        value: value,
+        httpOnly: false,
+        expires: cookieExpires,
+        domain: getCurrentHostname(),
+        path: '/',
+        secure: true,
+        sameSite: 'lax',
+    });
+}
+
+export async function setCredentialCookie(value: string) {
+    const cookieStore = await cookies();
+
+    cookieStore.set({
+        name: 'credential',
+        value: value,
+        httpOnly: true,
+        expires: cookieExpires,
+        domain: getCurrentHostname(),
+        path: '/',
+        secure: true,
+        sameSite: 'lax',
+    });
+}
+
 export async function bearerToken() {
     const cookieStore = await cookies();
 
