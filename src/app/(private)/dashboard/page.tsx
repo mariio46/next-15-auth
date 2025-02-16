b405ca810099a716870f6545ffbade60b8996495
Mario <mariomad2296@gmail.com>
2025-02-16 14:12:24 +0800

    Update page.tsx

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
 export default function DashboardPage() {
     return (
         <div>
             <h1>Dashboard Page</h1>
+            <Button asChild>
+                <Link href='/'>Home</Link>
+            </Button>
         </div>
     );
 }
