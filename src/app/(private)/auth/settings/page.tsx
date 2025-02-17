cb90f12f74a915634222e04c71e6ef92d8b3b702
Mario <mariomad2296@gmail.com>
2025-02-17 22:49:24 +0800

    Update page.tsx

--- src/app/(private)/auth/settings/page.tsx
+++ src/app/(private)/auth/settings/page.tsx
@@ -1,20 +1,43 @@
+import { Header, HeaderSubTitle, HeaderTitle } from '@/components/header';
+
 export default function SettingsPage() {
     return (
-        <>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nam, expedita asperiores aut
-                distinctio quasi non, illo sequi repudiandae doloribus voluptatum enim ipsam dicta impedit harum, in
-                magnam qui iste? Harum dicta corrupti eveniet necessitatibus ratione reprehenderit vel aperiam possimus
-                animi assumenda cum quis officia nisi laudantium enim dignissimos pariatur quasi quo eos sequi,
-                inventore a nostrum tempora. Iusto voluptatibus veritatis, cumque voluptate placeat aliquam id nobis
-                dicta optio sunt asperiores. Fugiat ullam aliquam consequuntur corporis exercitationem ratione libero
-                cum aspernatur omnis. Laudantium ut, doloribus ad fuga qui dolor illum esse, et maxime at dolorem
-                accusantium atque tenetur numquam sunt molestias tempore quibusdam perspiciatis quae nostrum, nobis
-                voluptates illo quo quas. Excepturi facilis similique, unde laboriosam reprehenderit deleniti est optio
-                dolorem pariatur esse officiis id? In totam praesentium dolores architecto ducimus ex reiciendis, sequi
-                nemo soluta at autem id sint, magni, non laudantium ipsa voluptatem esse illo veritatis velit
-                temporibus.
-            </p>
-        </>
+        <div className='divide-y divide-border [&>[data-slot=content]:first-child]:pb-6 [&>[data-slot=content]:not(:first-child)]:py-6'>
+            <section data-slot='content'>
+                <Header>
+                    <HeaderTitle>Account Information</HeaderTitle>
+                    <HeaderSubTitle className='max-w-xl'>
+                        Update your name and email to update your profile information.
+                    </HeaderSubTitle>
+                </Header>
+            </section>
+
+            <section data-slot='content'>
+                <Header>
+                    <HeaderTitle>Update Password</HeaderTitle>
+                    <HeaderSubTitle className='max-w-xl'>
+                        Manage your account by using the correct account and updating your password regularly.
+                    </HeaderSubTitle>
+                </Header>
+            </section>
+
+            <section data-slot='content'>
+                <Header>
+                    <HeaderTitle>Danger Area</HeaderTitle>
+                    <HeaderSubTitle className='max-w-xl'>
+                        Manage your account by using the correct account and updating your password regularly.
+                    </HeaderSubTitle>
+                </Header>
+            </section>
+
+            <section data-slot='content'>
+                <Header>
+                    <HeaderTitle>Theme Prefrence</HeaderTitle>
+                    <HeaderSubTitle className='max-w-xl'>
+                        Manage your account by using the correct account and updating your password regularly.
+                    </HeaderSubTitle>
+                </Header>
+            </section>
+        </div>
     );
 }
