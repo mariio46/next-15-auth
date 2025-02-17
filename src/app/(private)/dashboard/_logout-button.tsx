fde2f8338c1522f5293c66bd7932ec425add67b9
Mario <mariomad2296@gmail.com>
2025-02-17 14:23:16 +0800

    Update _logout-button.tsx

--- src/app/(private)/dashboard/_logout-button.tsx
+++ src/app/(private)/dashboard/_logout-button.tsx
@@ -1,30 +1,13 @@
 'use client';
 
-import * as React from 'react';
-
-import { useQueryClient } from '@tanstack/react-query';
 import { Loader } from 'lucide-react';
 
-import { logout } from '@/lib/server-actions';
-import { useAuthUserStore } from '@/stores/auth-user-store';
+import { useLogoutAction } from '@/actions/client/logout';
 
 import { Button } from '@/components/ui/button';
 
 const LogoutButton = () => {
-    const [loading, setLoading] = React.useState(false);
-
-    const queryClient = useQueryClient();
-
-    const setAuth = useAuthUserStore((state) => state.setAuth);
-
-    function handleLogout() {
-        setLoading((prevState) => !prevState);
-        logout();
-        setAuth({ user: undefined, status: 'unauthenticated' });
-        queryClient.removeQueries({
-            queryKey: ['auth-user'],
-        });
-    }
+    const { handleLogout, loading } = useLogoutAction();
 
     return (
         <Button onClick={handleLogout} disabled={loading}>
