79c7a16b2e9572809e5755102ab677142de0a400
Mario <mariomad2296@gmail.com>
2025-02-17 14:23:18 +0800

    Update client-utils.ts

--- src/lib/client-utils.ts
+++ src/lib/client-utils.ts
@@ -1,7 +1,27 @@
 import 'client-only';
 
+import { useQueryClient } from '@tanstack/react-query';
+
+import { useAuthUserStore } from '@/stores/auth-user-store';
+
 import { getCookie } from 'cookies-next/client';
 
 export function isLoggedIn(): boolean {
     return !!Number(getCookie('isLoggedIn') ?? 0);
+}
+
+export function redirectIfUnauthenticated() {
+    const setAuth = useAuthUserStore((state) => state.setAuth);
+    const queryClient = useQueryClient();
+
+    function handleIfUnauthenticated() {
+        queryClient.removeQueries({
+            queryKey: ['auth-user'],
+        });
+
+        setAuth({ user: undefined, status: 'unauthenticated' });
+
+        window.location.reload();
+    }
+    return { handleIfUnauthenticated };
 }
