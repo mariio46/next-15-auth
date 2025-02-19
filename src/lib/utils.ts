54abe41553862136978ded1785ab113f6b76e99f
Mario <mariomad2296@gmail.com>
2025-02-19 13:33:50 +0800

    Remove unused file and function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,9 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-import { axiosClient } from './axios';
-
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
-
-export const fetcher = (url: string) => axiosClient.get(url).then((res) => res.data);
 
 export function getCurrentHostname() {
     const hostname = process.env.NEXT_PUBLIC_BASE_APP_URL;
