e570e64c0c6c7f28c977194a6818d3b5bf8fa1d4
Mario <mariomad2296@gmail.com>
2025-02-19 12:55:25 +0800

    Update index.ts

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -1,10 +1,12 @@
+import type { AxiosError } from 'axios';
+
 type ApiResponse<TData> = {
     message: string;
     data: TData;
 };
 
-type ApiValidationErrorResponse<TError extends Record<string, string[]>> = {
+type ApiValidationErrorResponse<TError extends Record<string, string[]>> = AxiosError<{
     errors?: TError;
-};
+}>;
 
 export type { ApiResponse, ApiValidationErrorResponse };
