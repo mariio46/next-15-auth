6f4132a315720b42d40a52718dadb8410d53453b
Mario <mariomad2296@gmail.com>
2025-02-19 11:49:56 +0800

    Update layout.tsx

--- src/app/(private)/auth/layout.tsx
+++ src/app/(private)/auth/layout.tsx
@@ -1,11 +1,14 @@
 import { AuthSidebar } from '@/components/auth-sidebar';
+import { AuthShell } from '@/components/auth-sidebar/shell';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AuthSidebar />
-            <SidebarInset>{children}</SidebarInset>
+            <SidebarInset>
+                <AuthShell>{children}</AuthShell>
+            </SidebarInset>
         </SidebarProvider>
     );
 }
